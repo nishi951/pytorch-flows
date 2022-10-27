2b61408bc5796bd5b0e015da5a81f4d3bc62c1e4
Mark Nishimura <nishimuramarky@yahoo.com>
2022-10-26 23:20:37 -0700

    changing template experiment to be more basic

--- src/flow_utils/.template_exp/run.py
+++ src/flow_utils/.template_exp/run.py
@@ -4,20 +4,13 @@ from typing import Union, List, Tuple, Optional
 import tyro
 
 from flow_utils import SRC_ROOT
-from flow_utils.path_utils import ExpPathManager
 
 @dataclass
-class ExperimentConfig:
-    paths: ExpPathManager = field(
-        default_factory=lambda: ExpPathManager(
-            ExpPathManager.file2name(
-                __file__,
-                rel_dir=SRC_ROOT,
-            )
-        )
-    )
+class RunConfig:
+    name: str
+    """Name of this experiment"""
 
-def main(config: ExperimentConfig):
+def main(config: RunConfig):
     print(config)
 
 if __name__ == '__main__':
