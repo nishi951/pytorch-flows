10eadf618d836f20edcdd3c207992a91a010aa83
Mark Nishimura <nishimuramarky@yahoo.com>
2023-03-28 17:37:10 -0700

    4/n Cleaning up and adding test

--- src/pipeline_utils/test/decorator.py
+++ src/pipeline_utils/test/decorator.py
@@ -42,23 +42,41 @@ def step2(step1: int):
     print(f'Running step 2... (received step1: {step1})')
     return step1 + 3
 
+
 @pipeline.add(
-    deps=[step1, step2],
+    deps=[step1a, step2, step1b],
     cache=NpzCache('step3.npz',
                    load_callback=lambda x: x.item()),
 )
-def step3(step1: int, step2: int, step3_arg:int):
-    print(f'Running step 3... (received step1: {step1} and step2: {step2})')
-    return step1 + step2 + step3_arg
+def step3(step1a: int, step2: int, step1b: int, step3_arg:int):
+    print(f'Running step 3... (received step1: {step1a} and step2: {step2} and step1b: {step1b})')
+    return step1a + step2 + step1b + step3_arg
+
+
+@pipeline.add(deps=[step3])
+def step4a(step3):
+    print(f'Running step 4a... (received step3: {step3})')
+    return
+
+@pipeline.add(deps=[step2])
+def step4b(step2):
+    print(f'Running step 4b... (received step2: {step2})')
+    return
 
 def main():
     opt = tyro.cli(Config)
+    pipeline.setup(targets=opt.targets,
+                   reruns=opt.reruns)
+    pipeline.visualize()
 
-    step1_out = step1()
-    step2_out = step2(step1_out)
-    step3_out = step3(step1_out, step2_out, opt.step3_arg)
+    step1a_out = step1a()
+    step1b_out = step1b()
+    step2_out = step2(step1a_out)
+    step3_out = step3(step1a_out, step2_out, step1b_out, opt.step3_arg)
 
-    print(step3_out)
+    step4a(step3_out)
+    step4b(step2_out)
+
 
 if __name__ == '__main__':
     main()
